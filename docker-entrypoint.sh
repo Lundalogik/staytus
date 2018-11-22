765d3eef37111c194a0c73ef3553c9c9cc37b8c6
Martin Berg <mbrrg@users.noreply.github.com>
2018-11-22 15:57:33 +0100

    Update db creation.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@ if [ ! -z "$DEBUG" ]; then
     set -xe
 fi
 
+CREATE_DB="${CREATE_DB:-0}"
+
 # Database settings
 DB_ADAPTER="${DB_ADAPTER:-mysql2}"
 DB_POOL="${DB_POOL:-5}"
@@ -23,8 +25,8 @@ SETTING_STAYTUS_DEMO="${SETTING_STAYTUS_DEMO:-0}"
 CONFIG_FILE="/opt/staytus/config/environment.yml"
 
 prepareFiles() {
-    mv -f /opt/staytus/config/database.example.yml /opt/staytus/config/database.yml
-    mv -f /opt/staytus/config/environment.example.yml /opt/staytus/config/environment.yml
+    cp -f /opt/staytus/config/database.example.yml /opt/staytus/config/database.yml
+    cp -f /opt/staytus/config/environment.example.yml /opt/staytus/config/environment.yml
 }
 prepareDatabase() {
     local TIMEOUT=60
@@ -39,8 +41,6 @@ prepareDatabase() {
         echo -n "."
         sleep 1
     done
-    echo "CREATE DATABASE $DB_DATABASE CHARSET utf8 COLLATE utf8_unicode_ci;" | mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" || true
-    echo "Staytus database prepared."
 }
 configureDatabaseSettings() {
     sed -i "s/adapter:.*/adapter: \"$DB_ADAPTER\"/" /opt/staytus/config/database.yml
@@ -76,8 +76,19 @@ configureSettings() {
     echo "Staytus configuration succeeded."
 }
 runStaytus() {
-    cd /opt/staytus || exit 1
-    bundle exec rake staytus:build staytus:upgrade
+    cd /opt/staytus || exit 1    
+
+    bundle exec rake staytus:build
+
+    if [[ $CREATE_DB -eq "1" ]]; then
+        echo "Creating database."
+        
+        bundle exec rake db:create
+        bundle exec rake staytus:install
+    fi
+
+    bundle exec rake staytus:upgrade
+
     # Fix the Gemfile because https://github.com/bundler/bundler/issues/4576
     sed -i 's/\.\.\/\.\.\/Gemfile/Gemfile/g' /opt/staytus/bin/bundle
     exec -- bundle exec foreman start
